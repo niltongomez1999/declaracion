8226a76e95bb2a1121f4e65faca33060c758f490
niltongomez1999 <116848455+niltongomez1999@users.noreply.github.com>
2022-10-28 15:08:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const yesBtn  = document.querySelector('#yesBtn');
+
+
+
+
+yesBtn.addEventListener('click',function (){
+    alert('SABIA QUE IBAS A DECIR QUE SI CATHERIN AMOR MIO')
+});
+
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function () {
+      const randomX = parseInt(Math.random()+100);
+      const randomY = parseInt(Math.random()+100);
+    
+})
